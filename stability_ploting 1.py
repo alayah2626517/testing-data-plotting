2d70c4de55647c3c0fd126cea478511fe690f3b3
Hung Hsin-Yun <43771787+alayah2626517@users.noreply.github.com>
2025-02-07 14:50:59 +0800

    Update stability_ploting 1.py

--- stability_ploting 1.py
+++ stability_ploting 1.py
@@ -21,7 +21,7 @@ def run_app():
    
     ## 輸入框: Batch
     label_frame = tk.LabelFrame(root, text="Step 1", bg="light blue", bd=10, relief='groove')
-    label_frame.pack(padx=15, pady=10, pady=5)
+    label_frame.pack(padx=15, pady=10, fill="x")
     label_frame.pack_propagate(False)
     tk.Label(label_frame, text="Maximmum # of Batch ?", font=('Arial', 14), bg="light blue").grid(row=0, column=0, padx=55, pady=10)
     batch_num = tk.StringVar()
@@ -37,7 +37,7 @@ def run_app():
         else:
             print(f"Successfully selecting: {file_path}")
     label_frame_2 = tk.LabelFrame(root, width=380, height=100, text="Step 2", bg="DarkOliveGreen2", bd=10, relief='groove')
-    label_frame_2.pack(padx=10, fill="x")
+    label_frame_2.pack(padx=10, pady=10, fill="x")
     tk.Label(label_frame_2, text="Beware of the datasets layout!", font=('Arial', 12), bg="DarkOliveGreen2").grid(row=0, column=0, padx=25, pady=10)
     file_path = None
     tk.Button(label_frame_2, text="Select file", command=select_file).grid(row=1, column=0, padx=25, pady=10)
