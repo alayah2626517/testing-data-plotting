8b8553f712cdac665e50185a605cf11a13fc48f0
Hung Hsin-Yun <43771787+alayah2626517@users.noreply.github.com>
2025-02-07 14:50:11 +0800

    Update stability_ploting 1.py

--- stability_ploting 1.py
+++ stability_ploting 1.py
@@ -21,7 +21,7 @@ def run_app():
    
     ## 輸入框: Batch
     label_frame = tk.LabelFrame(root, text="Step 1", bg="light blue", bd=10, relief='groove')
-    label_frame.pack(padx=10, pady=5)
+    label_frame.pack(padx=15, pady=10, pady=5)
     label_frame.pack_propagate(False)
     tk.Label(label_frame, text="Maximmum # of Batch ?", font=('Arial', 14), bg="light blue").grid(row=0, column=0, padx=55, pady=10)
     batch_num = tk.StringVar()
