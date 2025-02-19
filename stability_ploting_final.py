323c9ae86464439758a7c29c337f6e0f32347e77
Hsin-Yun Hung <43771787+alayah2626517@users.noreply.github.com>
2025-02-19 15:28:43 +0800

    Update stability_ploting_final.py

--- stability_ploting_final.py
+++ stability_ploting_final.py
@@ -38,10 +38,10 @@ def run_app():
             print(f"Successfully selecting: {file_path}")
     label_frame_2 = tk.LabelFrame(root, width=380, height=100, text="Step 2", bg="DarkOliveGreen2", bd=10, relief='groove')
     label_frame_2.pack(padx=10, pady=5, fill="x")
-    tk.Label(label_frame_2, text="Select the file: ", font=('Arial', 12), bg="DarkOliveGreen2").grid(row=0, column=0, padx=66, pady=5)
-    tk.Label(label_frame_2, text="Beware of the datasets layout!", font=('Arial', 12, 'bold'), bg="DarkOliveGreen2").grid(row=1, column=0, padx=66, pady=5)
+    tk.Label(label_frame_2, text="Select the file: ", font=('Arial', 10), bg="DarkOliveGreen2").grid(row=0, column=0, padx=66, pady=5)
+    tk.Label(label_frame_2, text="Beware of the datasets layout!", font=('Arial', 11, 'bold'), bg="DarkOliveGreen2").grid(row=1, column=0, padx=66, pady=5)
     file_path = None
-    tk.Button(label_frame_2, text="Select file", command=select_file).grid(row=1, column=0, padx=45, pady=15)
+    tk.Button(label_frame_2, text="Select file", command=select_file).grid(row=2, column=0, padx=45, pady=15)
     
      ## 文件導入按鈕
     def load_sheet():
