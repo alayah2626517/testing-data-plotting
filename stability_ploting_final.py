e259c2ca97b905f478316e20a7dbe0275117ac59
Hsin-Yun Hung <43771787+alayah2626517@users.noreply.github.com>
2025-02-20 17:36:13 +0800

    Update stability_ploting_final.py

--- stability_ploting_final.py
+++ stability_ploting_final.py
@@ -71,6 +71,27 @@ def run_app():
             if end_idx != -1:
                 chart_y_label = unit_ori[:end_idx]
                 
+            ## 訂出小數位數的function
+            def get_decimal(value):
+                if isinstance(value, (int, float)):
+                    value_str = str(value).split(".")
+                    if len(value_str) > 1:
+                        return len(value_str[1].rstrip('0'))
+                    return 0
+                return 0
+                
+            def get_max_decimal(datasets):
+                max_decimal = 0
+                for row in datasets:
+                    for value in row[1:]:
+                        if isinstance(value, (int, float)) and value is not None:
+                            max_decimal = max(max_decimal, get_decimal(value))
+                return max_decimal
+
+            ## 訂出小數位數
+            max_decimal = get_max_decimal(datasets)
+
+            ##定義y軸上下限
             value_limit, value_limit_none, lower_limit, upper_limit = [], [], None, None
             if data_total[0][2] == "±":
                 lower_limit = data_total[0][1]-data_total[0][3]
@@ -100,26 +121,6 @@ def run_app():
                 value_limit_none = np.linspace(min_value, max_value, num=15)
                 value_limit_none = [round(value, max_decimal) for value in value_limit_none]
                 ax.set_yticks(value_limit_none)
-                
-            ## 訂出小數位數的function
-            def get_decimal(value):
-                if isinstance(value, (int, float)):
-                    value_str = str(value).split(".")
-                    if len(value_str) > 1:
-                        return len(value_str[1].rstrip('0'))
-                    return 0
-                return 0
-                
-            def get_max_decimal(datasets):
-                max_decimal = 0
-                for row in datasets:
-                    for value in row[1:]:
-                        if isinstance(value, (int, float)) and value is not None:
-                            max_decimal = max(max_decimal, get_decimal(value))
-                return max_decimal
-
-            ## 訂出小數位數
-            max_decimal = get_max_decimal(datasets)
                 
             ### 製作折線圖
             fig, ax = plt.subplots(1, 1, sharex='col', figsize=(10, 8))
