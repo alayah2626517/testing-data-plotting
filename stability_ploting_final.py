fef0ebe91f8521db1def284ed0b9c30dda02a03b
Hsin-Yun Hung <43771787+alayah2626517@users.noreply.github.com>
2025-02-20 15:39:48 +0800

    Update stability_ploting_final.py

--- stability_ploting_final.py
+++ stability_ploting_final.py
@@ -144,6 +144,7 @@ def run_app():
                 ax.set_yticks(value_limit)
             else:
                 all_values = [value for row in datasets for value in row[1:] if value is not None]
+                all_values = [round(value, max_decimal) for value in all_values]
                 min_value = min(all_values)
                 max_value = max(all_values)
                 ax.set_yticks(np.linspace(min_value, max_value, num=15))
